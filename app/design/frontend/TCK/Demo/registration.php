410d62c50f040a2e059c014ae62166d6dc7550be
Rodwyn Moreno <rodwynmoreno@gmail.com>
2017-06-15 13:06:40 -0500

    Registred theme.

---
+++ app/design/frontend/TCK/Demo/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2016 SW-THEMES. All rights reserved.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/TCK/Demo',
+    __DIR__
+);
